eeac552da37d8d85e99233a5eb9a98f1bc9ca55b
raiffaza <120578656+raiffaza@users.noreply.github.com>
2025-05-14 13:21:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ with col2:
     st.image("JPMorgan.png", width=250)
 
 # Title and Subtitle
-st.markdown("<h1 style='text-align: center;'>JPMorgan Loan Prediction App</h1>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center;'>Fast • Secure • Intelligent</h3>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>JPMorgan Loan Prediction App</h1>")
+st.markdown("<h3 style='text-align: center;'>Fast • Secure • Intelligent</h3>")
 
 # --- ABOUT JP MORGAN SECTION ---
 st.markdown("""
